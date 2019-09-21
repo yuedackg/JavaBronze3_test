6671ebdf9bb04f177e121fa3194538d36842a5f9
Yoshihiro UEDA <cqb00504.00@gmail.com>
2019-09-21 10:24:17 +0900

    fix

---
+++ P250.java
@@ -0,0 +1,20 @@
+public class P250{
+    private String size;
+    private String title;
+
+    P250 (){
+
+    }
+    P250( String title){
+        this.title = title;
+    }
+
+    void printInfo(){
+        System.out.println( title + " " + size);
+    }
+
+    public static void main( String[] args){
+        P250 p = new P250( );
+        p.printInfo();
+    }
+}
